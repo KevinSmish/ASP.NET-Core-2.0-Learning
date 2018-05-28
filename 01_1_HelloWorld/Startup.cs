aa0458f14c0c5ceb219ff711832f04dca76a7d76
KevinSmith <KevinSmish@users.noreply.github.com>
2018-05-28 20:55:33 +0300

    Added comment in classes

--- 01_1_HelloWorld/Startup.cs
+++ 01_1_HelloWorld/Startup.cs
@@ -11,10 +11,22 @@ namespace _01_1_HelloWorld
 {
     public class Startup
     {
+
+        // При запуске приложения сначала срабатывает конструктор, 
+        // затем метод ConfigureServices() и в конце метод Configure(). 
+        // Эти методы вызываются средой выполнения ASP.NET.
+
+        // Необязательный метод ConfigureServices() регистрирует сервисы, 
+        // которые используются приложением. В качестве параметра он принимает объект 
+        // IServiceCollection, который и представляет коллекцию сервисов в приложении. 
+        // С помощью методов расширений этого объекта производится конфигурация приложения 
+        // для использования сервисов. Все методы имеют форму Add[название_сервиса].
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
