44780b423fb2f190f62c17529273ee9e980e1efa
KevinSmith <KevinSmish@users.noreply.github.com>
2018-05-29 21:57:40 +0300

    Adding Validation

--- Ch02_PartyInvites/Controllers/HomeController.cs
+++ Ch02_PartyInvites/Controllers/HomeController.cs
@@ -27,8 +27,16 @@ namespace Ch02_PartyInvites.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            Repository.AddResponse(guestResponse);
-            return View("Thanks", guestResponse);
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
         }
 
         public ViewResult ListResponses()
