597b4b3200b095b22ef13546dd0dc4d846b9c886
KevinSmith <KevinSmish@users.noreply.github.com>
2018-05-30 21:09:02 +0300

    app.Use

--- 04_4_Use/Startup.cs
+++ 04_4_Use/Startup.cs
@@ -17,6 +17,8 @@ namespace _04_4_Use
         {
         }
 
+        /* Вариант 1 
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -24,9 +26,23 @@ namespace _04_4_Use
             int y = 8;
             int z = 0;
 
+            // В данном случае мы используем перегрузку метода Use, которая в качестве параметров
+            // принимает контекст запроса - объект HttpContext и делегат Func<Task>, который 
+            //представляет собой ссылку на следующий в конвейере компонент middleware.
+
+            // Метод app.Use реализует простейшую задачу - умножение двух чисел и затем передает
+            // обработку запроса следующим компонентам middleware в конвейере.
+
+            // То есть при вызове await next.Invoke() обработка запроса перейдет к тому компоненту,
+            // который установлен в методе app.Run()
+
             app.Use(async (context, next) =>
             {
                 z = x * y;
+
+                // Если бы мы не использовали вызов await next.Invoke() 
+                // или закомментировали бы его, то обращения к следующему компоненту 
+                // в конвейере не произошло бы.
                 await next.Invoke();
             });
 
@@ -34,6 +50,31 @@ namespace _04_4_Use
             {
                 await context.Response.WriteAsync($"x * y = {z}");
                 //await context.Response.WriteAsync("Hello World!");
+            });
+        }
+        */
+
+        // Вариант 2
+        public void Configure(IApplicationBuilder app)
+        {
+            int x = 2;
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("<p>Hello world!</p>");
+                x = x * 2;      // 2 * 2 = 4
+                await next.Invoke();    // вызов app.Run
+                x = x * 2;      // 8 * 2 = 16
+                await context.Response.WriteAsync($"Result: {x}");
+            });
+
+            app.Run(async (context) =>
+            {
+                // await Task.Delay(10000); можно поставить задержку
+                await context.Response.WriteAsync("<p>Good bye, World...</p>");
+
+                x = x * 2;  //  4 * 2 = 8
+                await Task.FromResult(0);
             });
         }
     }
