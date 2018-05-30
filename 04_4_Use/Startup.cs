6272ae5dc293ca93b5cdac1316dcb0b6cc59c19a
KevinSmith <KevinSmish@users.noreply.github.com>
2018-05-30 20:53:41 +0300

    New project for https://metanit.com/sharp/aspnet5/2.3.php

---
+++ 04_4_Use/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace _04_4_Use
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            int x = 5;
+            int y = 8;
+            int z = 0;
+
+            app.Use(async (context, next) =>
+            {
+                z = x * y;
+                await next.Invoke();
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"x * y = {z}");
+                //await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
