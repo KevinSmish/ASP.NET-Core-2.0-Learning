b828ac97ddd7cd6c3686dc9d140eeb6d61f8c954
KevinSmith <KevinSmish@users.noreply.github.com>
2018-05-31 21:56:14 +0300

    First commit for project

---
+++ 02_11_OWIN/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace _02_11_OWIN
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseOwin(pipeline =>
+            {
+                pipeline(next => SendResponseAsync);
+            });
+        }
+
+        public Task SendResponseAsync(IDictionary<string, object> environment)
+        {
+            // определяем ответ
+            string responseText = "Hello ASP.NET Core";
+            // кодируем его в массив байтов
+            byte[] responseBytes = Encoding.UTF8.GetBytes(responseText);
+
+            // получаем поток ответа
+            var responseStream = (Stream)environment["owin.ResponseBody"];
+            // отправка ответа
+            return responseStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+        }
+    }
+}
