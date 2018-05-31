2be948aef52a0ae1a7eeeef5ee062a59a16b278e
KevinSmith <KevinSmish@users.noreply.github.com>
2018-05-31 22:03:36 +0300

    owin.RequestHeaders

--- 02_11_OWIN/Startup.cs
+++ 02_11_OWIN/Startup.cs
@@ -30,8 +30,15 @@ namespace _02_11_OWIN
 
         public Task SendResponseAsync(IDictionary<string, object> environment)
         {
+            // получаем заголовки запроса
+            var requestHeaders = (IDictionary<string, string[]>)environment["owin.RequestHeaders"];
+
             // определяем ответ
-            string responseText = "Hello ASP.NET Core";
+            // string responseText = "Hello ASP.NET Core";
+
+            // получаем данные по User-Agent
+            string responseText = requestHeaders["User-Agent"][0];
+
             // кодируем его в массив байтов
             byte[] responseBytes = Encoding.UTF8.GetBytes(responseText);
 
@@ -40,5 +47,25 @@ namespace _02_11_OWIN
             // отправка ответа
             return responseStream.WriteAsync(responseBytes, 0, responseBytes.Length);
         }
+
+        /*
+        Ключи запроса
+        owin.RequestBody : объект Stream, представляющий тело запроса
+        owin.RequestHeaders : объект IDictionary<string, string[]>, представляющий заголовки запроса
+        owin.RequestMethod : объект string, представляющий метод запроса ("GET", "POST")
+        owin.RequestPath : объект string, представляет запрошенный путь относительно корня приложения
+        owin.RequestPathBase : объект string, содержащий часть запрошенного пути относительно корня приложения
+        owin.RequestProtocol : объект string, хранящий название протокола ("HTTP/1.0", "HTTP/1.1")
+        owin.RequestQueryString : объект string, хранящий строку запроса, то есть ту часть URI, которая идет после "?" (например, "foo=bar&mes=hello")
+        owin.RequestScheme : объект string, хранящий схему URI запроса ("http", "https")
+        owin.RequestId : объект string, представляющий уникальный идентификатор запроса (необязательный параметр)
+
+        Ключи ответа
+        owin.ResponseBody : объект Stream, представляющий поток ответа
+        owin.ResponseHeaders : объект IDictionary<string, string[]>, представляющий заголовки ответа
+        owin.ResponseStatusCode : объект int, хранящий статусный код HTTP, который посылается в ответе (необязательный параметр)
+        owin.ResponseReasonPhrase : объект string, содержащий словесное пояснение к статусному коду (необязательный параметр)
+        owin.ResponseProtocol : объект string, хранящий название протокола ("HTTP/1.0", "HTTP/1.1")
+        */
     }
 }
