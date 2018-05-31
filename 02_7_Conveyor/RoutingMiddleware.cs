097e0ff977906ed53959f8fd2206a41615efbb21
KevinSmith <KevinSmish@users.noreply.github.com>
2018-05-31 20:50:09 +0300

    Add some middlewares

---
+++ 02_7_Conveyor/RoutingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02_7_Conveyor
+{
+    public class RoutingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RoutingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string path = context.Request.Path.Value.ToLower();
+
+            if (path == "/index")
+            {
+                await context.Response.WriteAsync("Home Page");
+            }
+            else if (path == "/about")
+            {
+                await context.Response.WriteAsync("About");
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+            //await _next.Invoke(context);
+        }
+    }
+}
