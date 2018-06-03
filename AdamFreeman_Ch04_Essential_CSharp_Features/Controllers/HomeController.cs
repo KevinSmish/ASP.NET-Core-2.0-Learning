744a1c17f5197a8fa6e564b639352fe0bc1d0211
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-03 17:22:25 +0300

    Using an Index Initializer

--- AdamFreeman_Ch04_Essential_CSharp_Features/Controllers/HomeController.cs
+++ AdamFreeman_Ch04_Essential_CSharp_Features/Controllers/HomeController.cs
@@ -11,12 +11,23 @@ namespace AdamFreeman_Ch04_Essential_CSharp_Features.Controllers
             List<string> results = new List<string>();
             foreach (Product p in Product.GetProducts())
             {
-                string name = p?.Name;
-                decimal? price = p?.Price;
-                string relatedName = p?.Related?.Name;
+                // string name = p?.Name ?? "<No Name>";
+                // decimal? price = p?.Price ?? 0;
+                // string relatedName = p?.Related?.Name ?? "<None>";
 
-                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}",
-                    name, price, relatedName));
+                // Old style working with a string
+                //results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}",
+                //    name, price, relatedName));
+
+                // New style working with a string
+                //results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
+
+                Dictionary<string, Product> products = new Dictionary<string, Product> {
+                    ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
+                    ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
+                };
+                return View("Index", products.Keys);
+
             }
             return View(results);
         }
