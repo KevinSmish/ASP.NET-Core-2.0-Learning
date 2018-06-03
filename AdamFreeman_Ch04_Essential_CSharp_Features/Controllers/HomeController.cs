38a33b21a2f961f38711f68bb4a08925755eb33a
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-03 16:54:33 +0300

    First position (page 71 of Freeman A. - Pro ASP.NET Core MVC 2, Seventh Edition - 2017)

---
+++ AdamFreeman_Ch04_Essential_CSharp_Features/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdamFreeman_Ch04_Essential_CSharp_Features.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View(new string[] { "C#", "Language", "Features" });
+        }
+    }
+}
