ed49d8759ad99112181540102db6fbc16b2c3070
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-10 22:01:53 +0300

    Xunit PropertyOnceFakeRepository

--- AdamFreeman_Ch07_XUnitTestProject/HomeControllerTests.cs
+++ AdamFreeman_Ch07_XUnitTestProject/HomeControllerTests.cs
@@ -13,63 +13,81 @@ namespace AdamFreeman_Ch07_XUnitTestProject
     {
         class ModelCompleteFakeRepository : IRepository
         {
-            public IEnumerable<Product> Products { get; } = new Product[] {
-                    new Product { Name = "P1", Price = 275M },
-                    new Product { Name = "P2", Price = 48.95M },
-                    new Product { Name = "P3", Price = 19.50M },
-                    new Product { Name = "P3", Price = 34.95M }
-            };
-
+            public IEnumerable<Product> Products { get; set; }
             public void AddProduct(Product p)
             {
                 // do nothing - not required for test
             }
         }
 
-        [Fact]
-        public void IndexActionModelIsComplete()
+        [Theory]
+        /*
+        [InlineData(275, 48.95, 19.50, 24.95)]
+        [InlineData(5, 48.95, 19.50, 24.95)]
+
+        public void IndexActionModelIsComplete(decimal price1, decimal price2,
+            decimal price3, decimal price4)
         {
             // Arrange
             var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepository();
+            controller.Repository = new ModelCompleteFakeRepository
+            {
+                Products = new Product[] {
+                        new Product {Name = "P1", Price = price1 },
+                        new Product {Name = "P2", Price = price2 },
+                        new Product {Name = "P3", Price = price3 },
+                        new Product {Name = "P4", Price = price4 },
+                }
+            };
+            */
+
+        [ClassData(typeof(ProductTestData))]
+        public void IndexActionModelIsComplete(Product[] products)
+        {
+            // Arrange
+            var controller = new HomeController();
+            controller.Repository = new ModelCompleteFakeRepository
+            {
+                Products = products
+            };
 
             // Act
             var model = (controller.Index() as ViewResult)?.ViewData.Model
-            as IEnumerable<Product>;
+                as IEnumerable<Product>;
+    
             // Assert
             Assert.Equal(controller.Repository.Products, model,
                 Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name
                 && p1.Price == p2.Price));
         }
 
-        class ModelCompleteFakeRepositoryPricesUnder50 : IRepository
+        // --------------------------------------------------------------------
+        class PropertyOnceFakeRepository : IRepository
         {
-            public IEnumerable<Product> Products { get; } = new Product[] {
-                    new Product { Name = "P1", Price = 5M },
-                    new Product { Name = "P2", Price = 48.95M },
-                    new Product { Name = "P3", Price = 19.50M },
-                    new Product { Name = "P3", Price = 34.95M }
-            };
-
+            public int PropertyCounter { get; set; } = 0;
+            public IEnumerable<Product> Products
+            {
+                get
+                {
+                    PropertyCounter++;
+                    return new[] { new Product { Name = "P1", Price = 100 } };
+                }
+            }
             public void AddProduct(Product p)
             {
                 // do nothing - not required for test
             }
         }
-
         [Fact]
-        public void IndexActionModelIsCompletePricesUnder50()
+        public void RepositoryPropertyCalledOnce()
         {
             // Arrange
-            var controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepositoryPricesUnder50();
+            var repo = new PropertyOnceFakeRepository();
+            var controller = new HomeController { Repository = repo };
             // Act
-            var model = (controller.Index() as ViewResult)?.ViewData.Model
-            as IEnumerable<Product>;
+            var result = controller.Index();
             // Assert
-            Assert.Equal(controller.Repository.Products, model,
-            Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name
-            && p1.Price == p2.Price));
+            Assert.Equal(1, repo.PropertyCounter);
         }
     }
 }
