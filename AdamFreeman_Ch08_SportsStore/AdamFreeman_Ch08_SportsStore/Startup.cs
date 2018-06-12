143e1bfa579c3a0e0f55c31db8ebb810d854930f
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-12 13:40:07 +0300

    First commit

--- AdamFreeman_Ch08_SportsStore/AdamFreeman_Ch08_SportsStore/Startup.cs
+++ AdamFreeman_Ch08_SportsStore/AdamFreeman_Ch08_SportsStore/Startup.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AdamFreeman_Ch08_SportsStore.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,23 +15,20 @@ namespace AdamFreeman_Ch08_SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //if (env.IsDevelopment())
-            app.UseDeveloperExceptionPage();
-            app.UseStatusCodePages();
-            app.UseStaticFiles();
-            app.UseMvc(routes => {
-                routes.MapRoute(
-                name: "default",
-                template: "{controller=Product}/{action=List}/{id?}");
-            });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
         }
     }
 }
