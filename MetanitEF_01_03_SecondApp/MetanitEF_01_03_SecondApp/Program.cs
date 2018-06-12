ea86d53d87daf593aafcc71051bb861b314f2a30
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-12 15:54:43 +0300

    First commit

---
+++ MetanitEF_01_03_SecondApp/MetanitEF_01_03_SecondApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MetanitEF_01_03_SecondApp
+{
+    // ToDo
+    // Для работы с существующей БД MS SQL Server нам надо добавить три пакета:
+
+    // Install-Package Microsoft.EntityFrameworkCore.SqlServer
+    // Install-Package Microsoft.EntityFrameworkCore.Tools
+    // Install-Package Microsoft.EntityFrameworkCore.SqlServer.Design
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
