6718ad1d9bc077098020be5be477dcf24ddfb99b
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-12 15:09:05 +0300

    Added data

--- MetanitEF_01_02_FirstApp/MetanitEF_01_02_FirstApp/Program.cs
+++ MetanitEF_01_02_FirstApp/MetanitEF_01_02_FirstApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 // ToDo
 // 1. To add Microsoft.EntityFrameworkCore.SqlServer
@@ -12,7 +13,27 @@ namespace MetanitEF_01_02_FirstApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                // создаем два объекта User
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                // добавляем их в бд
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.Read();
         }
     }
 }
