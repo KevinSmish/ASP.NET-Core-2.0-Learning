b9766279bf75feabcb4e85656deae6a4ad83f899
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-16 21:35:35 +0300

    no

--- AdamFreeman_Ch08_SportsStore/AdamFreeman_Ch08_SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ AdamFreeman_Ch08_SportsStore/AdamFreeman_Ch08_SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 namespace AdamFreeman_Ch08_SportsStore.Infrastructure
 {
     [HtmlTargetElement("div", Attributes = "page-model")]
-    public class PageLinkTagHelper: TagHelper 
+    public class PageLinkTagHelper : TagHelper
     {
         private IUrlHelperFactory urlHelperFactory;
 
@@ -29,8 +29,13 @@ namespace AdamFreeman_Ch08_SportsStore.Infrastructure
 
         public string PageAction { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
         public override void Process(TagHelperContext context,
-            TagHelperOutput output)
+                TagHelperOutput output)
         {
             IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
             TagBuilder result = new TagBuilder("div");
@@ -38,11 +43,18 @@ namespace AdamFreeman_Ch08_SportsStore.Infrastructure
             {
                 TagBuilder tag = new TagBuilder("a");
                 tag.Attributes["href"] = urlHelper.Action(PageAction,
-                    new { productPage = i });
+                   new { productPage = i });
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
                 tag.InnerHtml.Append(i.ToString());
                 result.InnerHtml.AppendHtml(tag);
             }
             output.Content.AppendHtml(result.InnerHtml);
         }
     }
+
 }
--- AdamFreeman_Ch08_SportsStore/AdamFreeman_Ch08_SportsStore/Models/ViewModels/PagingInfo.cs
+++ AdamFreeman_Ch08_SportsStore/AdamFreeman_Ch08_SportsStore/Models/ViewModels/PagingInfo.cs
@@ -10,6 +10,7 @@ namespace AdamFreeman_Ch08_SportsStore.Models.ViewModels
         public int TotalItems { get; set; }
         public int ItemsPerPage { get; set; }
         public int CurrentPage { get; set; }
+
         public int TotalPages =>
             (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
     }
