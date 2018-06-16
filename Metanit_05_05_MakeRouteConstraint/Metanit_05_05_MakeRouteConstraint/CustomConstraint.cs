f7c79dffd8475e57aecbf98681e21fc82be5027f
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-16 16:14:44 +0300

    a first commit

---
+++ Metanit_05_05_MakeRouteConstraint/Metanit_05_05_MakeRouteConstraint/CustomConstraint.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metanit_05_05_MakeRouteConstraint
+{
+   
+    public class CustomConstraint : Microsoft.AspNetCore.Routing.IRouteConstraint
+    {
+        private string uri;
+        public CustomConstraint(string uri)
+        {
+            this.uri = uri;
+        }
+
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, 
+            RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            return !(uri == httpContext.Request.Path);
+        }
+    }
+}
---
+++ Metanit_05_05_MakeRouteConstraint/Metanit_05_05_MakeRouteConstraint/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Metanit_05_05_MakeRouteConstraint
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var trackPackageRouteHandler = new RouteHandler(context =>
+            {
+                var routeValues = context.GetRouteData().Values;
+                return context.Response.WriteAsync(
+                    $"Hello! Route values: {string.Join(", ", routeValues)}");
+            });
+
+            var routeBuilder = new RouteBuilder(app, trackPackageRouteHandler);
+
+            routeBuilder.MapRoute("default",
+                "{controller}/{action}/{id?}",
+                null,
+                new { myConstraint = new CustomConstraint("/Home/Index/12") }
+            );
+
+            // for example: http://localhost:55065/Home/Index/12
+
+            // Теперь запрос /Home/Index/12 не будет обрабатываться, даже если он 
+            // удовлетворяет всем остальным условиям и ограничениям
+
+            var routes = routeBuilder.Build();
+            app.UseRouter(routes);
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
