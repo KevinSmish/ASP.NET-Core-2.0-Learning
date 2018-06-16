c962c865e3a966a1251a74d8e236350851be2401
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-16 20:28:56 +0300

    Adding the Tag Helper Class

--- AdamFreeman_Ch08_SportsStore/AdamFreeman_Ch08_SportsStore/Controllers/ProductController.cs
+++ AdamFreeman_Ch08_SportsStore/AdamFreeman_Ch08_SportsStore/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using AdamFreeman_Ch08_SportsStore.Models;
+using AdamFreeman_Ch08_SportsStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -21,10 +22,19 @@ namespace AdamFreeman_Ch08_SportsStore.Controllers
         // http://localhost:62638/?productPage=2
 
         public ViewResult List(int productPage = 1)
-            => View(repository.Products
-                .OrderBy(p => p.ProductID)
-                .Skip((productPage - 1) * PageSize)
-                .Take(PageSize));
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
 
     }
 }
---
+++ AdamFreeman_Ch08_SportsStore/AdamFreeman_Ch08_SportsStore/Controllers/ProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using AdamFreeman_Ch08_SportsStore.Models;
+using AdamFreeman_Ch08_SportsStore.Models.ViewModels;
+
+namespace AdamFreeman_Ch08_SportsStore.Controllers
+{
+    internal class ProductsListViewModel
+    {
+        public IQueryable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
