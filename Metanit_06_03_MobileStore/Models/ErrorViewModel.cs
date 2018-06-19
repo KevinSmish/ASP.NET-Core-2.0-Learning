05d4c7c77eb982efc4fdb7319909ee99c7d7924c
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-19 20:40:24 +0300

    my first commit for project

---
+++ Metanit_06_03_MobileStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Metanit_06_03_MobileStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
