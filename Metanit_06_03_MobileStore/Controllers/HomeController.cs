6c38cfa094c2728497fb0762fc7517a014a53e78
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-19 20:55:56 +0300

    added functional for buying

--- Metanit_06_03_MobileStore/Controllers/HomeController.cs
+++ Metanit_06_03_MobileStore/Controllers/HomeController.cs
@@ -20,6 +20,22 @@ namespace Metanit_06_03_MobileStore.Controllers
             return View(db.Phones.ToList());
         }
 
+        [HttpGet]
+        public IActionResult Buy(int id)
+        {
+            ViewBag.PhoneId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
