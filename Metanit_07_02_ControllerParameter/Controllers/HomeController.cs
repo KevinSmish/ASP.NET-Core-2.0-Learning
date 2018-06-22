a0ee8ca934dd3eafaaef1283feb0a16fd8b5f57c
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-22 21:40:17 +0300

    Added Request and Response

--- Metanit_07_02_ControllerParameter/Controllers/HomeController.cs
+++ Metanit_07_02_ControllerParameter/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Metanit_07_02_ControllerParameter.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
@@ -123,6 +124,24 @@ namespace Metanit_07_02_ControllerParameter.Controllers
             string file_name = "book.txt";
             return PhysicalFile(file_path, file_type, file_name);
         }
+
+        // https://localhost:44324/Home/IndexSix
+        public void IndexSix()
+        {
+            string table = "";
+            foreach (var header in Request.Headers)
+            {
+                table += $"<tr><td>{header.Key}</td><td>{header.Value}</td></tr>";
+            }
+            Response.WriteAsync(String.Format("<table>{0}</table>", table));
+        }
+
+        // https://localhost:44324/Home/IndexSeven
+        public void IndexSeven()
+        {
+            Response.StatusCode = 404;
+            Response.WriteAsync("Ресурс не найден");
+        }
     }
 
     public class Geometry
@@ -142,3 +161,29 @@ namespace Metanit_07_02_ControllerParameter.Controllers
         public int Age { get; set; }
     }
 }
+
+/*
+Объект HttpContext инкапсулирует всю информацию о запросе. В частности, он определяет следующие свойства:
+
+    Request: содержит собственно информацию о текущем запросе.
+    Response: управляет ответом
+    User: представляет текущего пользователя, который обращается к приложению
+    Session: объект для работы с сессиями
+
+Свойство HttpContext.Request представляет объект HttpRequest и предоставляет разнообразную информацию о запросе. Этот же объект доступен через свойство Request класса Conroller. Среди свойств объекта Request можно выделить следующие:
+    Body: объект Stream, который используетя для чтения данных запроса
+    Cookies: куки, полученные в запросе
+    Form: коллекция значений отправленных форм
+    Headers: коллекция заголовков запроса
+    Path: возвращает запрошенный путь - строка запроса без домена и порта
+    Query: возвращает коллекцию переданных через строку запроса параметров
+    QueryString: возвращает ту часть запроса, которая содержит параметры. Например, в запросе http://localhost:52682/Home/Index?alt=4 это будет ?alt=4
+
+Свойство HttpContext.Response представляет объект HttpResponse и позволяет управлять ответом на запрос, в частности, устанавливать заголовки ответа, куки, отправлять в выходной поток некоторый ответ. Этот же объект доступен через свойство Response класса Conroller. Среди свойств объекта Response можно выделить следующие:
+    Body: объект Stream, который применяется для отправки данных в ответ пользователю
+    Cookies: куки, отправляемые в ответе
+    ContentType: MIME-тип ответа
+    Headers: коллекция заголовков ответа
+    StatusCode: статусный код ответа
+
+ */
