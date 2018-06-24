b6afc748783b20806f4f3a11128848a3502e45dd
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-24 21:09:55 +0300

    the first commit

---
+++ Metanit_08_08_ViewForms/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metanit_08_08_ViewForms.Controllers
+{
+    // https://localhost:44322/home/login
+
+    public class HomeController: Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string login, string password)
+        {
+            string authData = $"Login: {login}   Password: {password}";
+            return Content(authData);
+        }
+    }
+}
