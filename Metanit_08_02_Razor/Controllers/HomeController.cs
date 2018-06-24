dae435c5655cd957dac360a9e9cb89a073750232
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-24 18:32:06 +0300

    my first commit

---
+++ Metanit_08_02_Razor/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Metanit_08_02_Razor.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metanit_08_02_Razor.Controllers
+{
+    public class HomeController: Controller
+    {
+        private readonly IHostingEnvironment _appEnvironment;
+        public HomeController(IHostingEnvironment appEnvironment) 
+        {
+            _appEnvironment = appEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            Product[] array = {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+            return View(array);
+        }
+
+        // https://localhost:44344/home/about
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Hello ASP.NET Core";
+            return View();
+        }
+    }
+}
