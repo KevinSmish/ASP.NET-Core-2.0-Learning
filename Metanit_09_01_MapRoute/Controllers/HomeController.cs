08e2d71e8265d574137a73736eb1a48ecdb16950
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-28 21:07:09 +0300

    my first commit

---
+++ Metanit_09_01_MapRoute/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metanit_09_01_MapRoute.Controllers
+{
+    public class HomeController : Controller
+    {
+        // https://localhost:44372/homepage
+        [Route("homepage")]
+        [Route("Main")]     // сопоставляется с homepage либо с Main
+        public IActionResult Index()
+        {
+            return Content("Hello ASP.NET MVC 6");
+        }
+
+        // http://localhost:44372/10/lumia
+        [Route("{id:int}/{name:maxlength(10)}")]
+        public IActionResult Test(int id, string name)
+        {
+            return Content($" id={id} | name={name}");
+        }
+
+        [Route("[controller]/[action]")]
+        public IActionResult IndexOne()
+        {
+            var controller = RouteData.Values["controller"].ToString();
+            var action = RouteData.Values["action"].ToString();
+            return Content($"controller: {controller} | action: {action}");
+        }
+
+        /* 
+            [Route("main/store/{name}")]
+            public IActionResult Index(string name)
+            {
+                return Content(name);
+            }
+            [Route("main/{id:int}/{name:maxlength(10)}")]
+            public IActionResult Test(int id, string name)
+            {
+                return Content($" id={id} | name={name}");
+            }
+        */
+    }
+}
---
+++ Metanit_09_01_MapRoute/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Metanit_09_01_MapRoute
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Далее метод не работает, поскольку в классе контроллера определен атрибут [Route("homepage")]
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("api", "api/get", new { controller = "Home", action = "About" });
+
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
