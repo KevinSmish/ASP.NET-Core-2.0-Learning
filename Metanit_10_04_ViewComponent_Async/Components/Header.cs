f56ea08f2623268d113053fcccf8020315506c1f
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-29 21:32:49 +0300

    my first commit

---
+++ Metanit_10_04_ViewComponent_Async/Components/Header.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metanit_10_04_ViewComponent_Async.Components
+{
+    public class Header : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string htmlContent = String.Empty;
+            using (FileStream fileStream = new FileStream("Files/header.html", FileMode.Open))
+            {
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    htmlContent = await reader.ReadToEndAsync();
+                }
+            }
+            return new HtmlContentViewComponentResult(new HtmlString(htmlContent));
+        }
+    }
+}
