08f1841b932a121c935dd9f10736128d8f37026c
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-29 21:09:27 +0300

    my first commit

---
+++ Metanit_10_03_ViewComponentResult/Components/PhonesList.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metanit_10_03_ViewComponentResult.Components
+{
+    public class PhonesList : ViewComponent
+    {
+        Dictionary<string, int> phones;
+
+        public PhonesList()
+        {
+            phones = new Dictionary<string, int>
+            {
+                {"iPhone 7", 56000 },
+                {"Alcatel Idol S4", 26000 },
+                {"Samsung Galaxy S7", 50000 },
+                {"HP Elite x3", 56000 },
+                {"Xiaomi Mi5S", 22000 },
+                {"Meizu Pro 6", 22000 },
+                {"Huawei Honor 8", 23000 },
+                {"Google Pixel", 30000 }
+            };
+        }
+
+        public IViewComponentResult Invoke(int maxPrice)
+        {
+            var items = phones.Where(p => p.Value <= maxPrice).ToList();
+            return View(items);
+        }
+    }
+}
