a328614023dede72843003d19ab70d42e1459703
KevinSmith <KevinSmish@users.noreply.github.com>
2018-06-29 21:46:56 +0300

    my first commit

---
+++ Metanit_10_05_ViewComponentContext/Components/PhonesList.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metanit_10_05_ViewComponentContext.Components
+{
+    public class PhonesList : ViewComponent
+    {
+        Dictionary<string, int> phones;
+        public PhonesList()
+        {
+            phones = new Dictionary<string, int>
+            {
+                {"iPhone 7", 56000 },
+                {"Alcatel Idol S4", 26000 },
+                {"Samsung Galaxy S7", 50000 },
+                {"HP Elite x3", 56000 },
+                {"Xiaomi Mi5S", 22000 },
+                {"Meizu Pro 6", 22000 },
+                {"Huawei Honor 8", 23000 },
+                {"Google Pixel", 30000 }
+            };
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            int maxPrice = phones.Max(x => x.Value);
+
+            // если передан параметр id
+            if (RouteData.Values.ContainsKey("id"))
+                Int32.TryParse(RouteData.Values["id"]?.ToString(), out maxPrice);
+
+            ViewBag.Phones = phones.Where(p => p.Value <= maxPrice).ToList();
+            ViewData["Header"] = $"Список смартфонов с ценой от {maxPrice.ToString("c")} и меньше";
+
+            return View();
+        }
+    }
+}
---
+++ Metanit_10_05_ViewComponentContext/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+/*
+ Если мы создаем компонент с помощью наследования от класса ViewComponent, 
+ то нам становится доступным контекст компонента через ряд свойств:
+
+    HttpContext: представляет контекст, который описывает полученный запрос, а также отправляемый ответ
+
+    ModelState: представляет состояние модели в виде объекта ModelStateDictionary
+
+    Request: возвращает контекст запроса в виде объекта HttpRequest
+
+    RouteData: возвращает данные маршрута
+
+    Url: представляет объект IUrlHelper, который используется для генерации адресов URL
+
+    User: представляет текущего пользователя в виде объкта IPrincipal
+
+    ViewBag: представляет динамический объект, который может использоваться для передачи 
+        данных в представление
+
+    ViewContext: описывает контекст главного представления, в котором вызывается компонент
+
+    ViewComponentContext: представляет объект ViewComponentContext, который инкапсулирует 
+        контекст компонента
+
+    ViewData: возвращает объект ViewDataDictionary, который применяется для передачи данных
+        в представление
+
+*/
+namespace Metanit_10_05_ViewComponentContext
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // https://localhost:44387/Home/Index/35000
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
