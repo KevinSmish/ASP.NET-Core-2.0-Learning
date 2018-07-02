ce3e50fa15f7759c5d31c0409e97dd7ad2846707
KevinSmith <KevinSmish@users.noreply.github.com>
2018-07-02 21:15:29 +0300

    my first commit

---
+++ Metanit_13_TagHelpers/Metanit_13_01_TagHelper_Sample/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+/*
+    AnchorTagHelper представляет тег-хелпер, который позволяет создавать ссылки. 
+    Он может принимать ряд специальных атрибутов:
+        asp-controller: указывает на контроллер, которому предназначен запрос
+        asp-action: указывает на действие контроллера
+        asp-host: указывает на домен сайта
+        asp-protocol: определяет протокол (http или https)
+        asp-route: указывает на название маршрута
+        asp-all-route-data: устанавливает набор значений для параметров
+        asp-route-[название параметра]: определяет значение для определенного параметра
+        asp-fragment: определяет ту часть хэш-ссылки, которая идет после символа решетки #. 
+            Например, "paragraph2" в ссылке "http://mysite.com/#paragraph2"
+*/
+
+namespace Metanit_13_01_TagHelper_Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
