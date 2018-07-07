d63ac418de7c8d72cc9f0987144d22f6bbb72eae
KevinSmith <KevinSmish@users.noreply.github.com>
2018-07-07 16:40:52 +0300

    added DataAnnotations

---
+++ Metanit_14_Validation/Metanit_14_06_DataAnnotations/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Metanit_14_06_DataAnnotations.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metanit_14_06_DataAnnotations.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Person p = new Person
+            {
+                Name = "Элронд Смит",
+                DateOfBirth = new DateTime(2000, 7, 24),
+                HomePage = "www.microsoft.com",
+                Email = "elrond.smith@gmail.com",
+                Password = "qwerty"
+            };
+            return View(p);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            return View(person);
+        }
+    }
+}
