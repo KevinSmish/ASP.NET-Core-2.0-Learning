0af0b57c08f3908f65cc308709f914ce45ad8675
KevinSmith <KevinSmish@users.noreply.github.com>
2018-07-09 22:14:39 +0300

    added a TagHelper-class for sort

--- Metanit_15_EntityFramework/Metanit_15_04_EntityFrameworkSort/Controllers/HomeController.cs
+++ Metanit_15_EntityFramework/Metanit_15_04_EntityFrameworkSort/Controllers/HomeController.cs
@@ -37,6 +37,39 @@ namespace Metanit_15_04_EntityFrameworkSort.Controllers
         {
             IQueryable<User> users = db.Users.Include(x => x.Company);
 
+            switch (sortOrder)
+            {
+                case SortState.NameDesc:
+                    users = users.OrderByDescending(s => s.Name);
+                    break;
+                case SortState.AgeAsc:
+                    users = users.OrderBy(s => s.Age);
+                    break;
+                case SortState.AgeDesc:
+                    users = users.OrderByDescending(s => s.Age);
+                    break;
+                case SortState.CompanyAsc:
+                    users = users.OrderBy(s => s.Company.Name);
+                    break;
+                case SortState.CompanyDesc:
+                    users = users.OrderByDescending(s => s.Company.Name);
+                    break;
+                default:
+                    users = users.OrderBy(s => s.Name);
+                    break;
+            }
+            IndexViewModel viewModel = new IndexViewModel
+            {
+                Users = await users.AsNoTracking().ToListAsync(),
+                SortViewModel = new SortViewModel(sortOrder)
+            };
+            return View(viewModel);
+        }
+        /*
+        public async Task<IActionResult> Index(SortState sortOrder = SortState.NameAsc)
+        {
+            IQueryable<User> users = db.Users.Include(x => x.Company);
+
             ViewData["NameSort"] = sortOrder == SortState.NameAsc ? SortState.NameDesc : SortState.NameAsc;
             ViewData["AgeSort"] = sortOrder == SortState.AgeAsc ? SortState.AgeDesc : SortState.AgeAsc;
             ViewData["CompSort"] = sortOrder == SortState.CompanyAsc ? SortState.CompanyDesc : SortState.CompanyAsc;
@@ -64,6 +97,7 @@ namespace Metanit_15_04_EntityFrameworkSort.Controllers
             }
             return View(await users.AsNoTracking().ToListAsync());
         }
+        */
 
         /*
         public IActionResult Index()
---
+++ Metanit_15_EntityFramework/Metanit_15_04_EntityFrameworkSort/Models/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metanit_15_04_EntityFrameworkSort.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<User> Users { get; set; }
+        public SortViewModel SortViewModel { get; set; }
+    }
+}
