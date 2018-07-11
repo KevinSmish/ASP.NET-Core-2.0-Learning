d8c4f8dd4c80bcaea99ad730abc5cbce21e5b00a
KevinSmith <KevinSmish@users.noreply.github.com>
2018-07-11 21:25:07 +0300

    added preproc Less ans Sass

---
+++ Metanit_16_Clients/Metanit_16_06_Less/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Metanit_16_06_Less.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Metanit_16_Clients/Metanit_16_06_Sass/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Metanit_16_06_Sass.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
