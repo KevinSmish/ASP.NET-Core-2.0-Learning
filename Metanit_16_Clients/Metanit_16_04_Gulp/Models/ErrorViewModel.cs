a32f7120f03308db50e347397594c3b484e00530
KevinSmith <KevinSmish@users.noreply.github.com>
2018-07-11 20:27:43 +0300

    my first commit

---
+++ Metanit_16_Clients/Metanit_16_04_Gulp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Metanit_16_04_Gulp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
