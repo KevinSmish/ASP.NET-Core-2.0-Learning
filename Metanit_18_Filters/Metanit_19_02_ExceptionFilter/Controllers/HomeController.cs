82f61b3ab637556748ccc660985313b5ceab5780
KevinSmith <KevinSmish@users.noreply.github.com>
2018-07-13 20:27:11 +0300

    added ExceptionFilter-project

---
+++ Metanit_18_Filters/Metanit_19_02_ExceptionFilter/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Metanit_19_02_ExceptionFilter.Models;
+using Metanit_19_02_ExceptionFilter.Filters;
+
+namespace Metanit_19_02_ExceptionFilter.Controllers
+{
+    public class HomeController : Controller
+    {
+        [CustomExceptionFilter]
+        public IActionResult Index()
+        {
+            int x = 0;
+            int y = 8 / x;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Metanit_18_Filters/Metanit_19_02_ExceptionFilter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Metanit_19_02_ExceptionFilter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
