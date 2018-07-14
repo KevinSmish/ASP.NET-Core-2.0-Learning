a8a0db75979252e3eae49a784e33905eb5a128d8
KevinSmith <KevinSmish@users.noreply.github.com>
2018-07-14 16:00:44 +0300

    my first commit

---
+++ 180714_LINQ_Samples/180714_LINQ_Samples/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _180714_LINQ_Samples
+{
+    class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+}
