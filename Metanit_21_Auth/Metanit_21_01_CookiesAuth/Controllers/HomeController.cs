be227233d34b5c6b55db788002f6ab130a0176f3
KevinSmith <KevinSmish@users.noreply.github.com>
2018-07-17 22:34:14 +0300

    added [AllowAnonymous]

--- Metanit_21_Auth/Metanit_21_01_CookiesAuth/Controllers/HomeController.cs
+++ Metanit_21_Auth/Metanit_21_01_CookiesAuth/Controllers/HomeController.cs
@@ -9,14 +9,26 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Metanit_21_01_CookiesAuth.Controllers
 {
+
+    // https://localhost:44362/home/about
+
+    // Здесь доступ ко всей функциональности контроллера Home имеют только аутентифицированные 
+    // пользователи, за исключением доступа к методу About: к нему могут обращаться абсолютно 
+    // все пользователи приложения.
+
+    [Authorize]
     public class HomeController : Controller
     {
-        [Authorize]
         public IActionResult Index()
         {
-            return Content(User.Identity.Name);
+            if (User.Identity.IsAuthenticated)
+            {
+                return Content(User.Identity.Name);
+            }
+            return Content("не аутентифицирован");
         }
 
+        [AllowAnonymous]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
