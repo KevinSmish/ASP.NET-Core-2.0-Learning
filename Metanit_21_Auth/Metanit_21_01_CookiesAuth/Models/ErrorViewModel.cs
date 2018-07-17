af0b5ab416bd9bb98e0826d069564a15b9bcf0e0
KevinSmith <KevinSmish@users.noreply.github.com>
2018-07-17 22:27:27 +0300

    my first commit

---
+++ Metanit_21_Auth/Metanit_21_01_CookiesAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Metanit_21_01_CookiesAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
